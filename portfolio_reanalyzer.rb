d25768482f3cf62203690d1a75e7a106877ed48b
Wolfgang Mueller <mueller@md-phw.de>
2016-02-26 16:42:45 +0100

    running skeleton for reloading locally stored member data and views for further analysis

--- portfolio_reanalyzer.rb
+++ portfolio_reanalyzer.rb
@@ -16,12 +16,14 @@
 
 require 'rubygems'
 require 'fileutils'
+require 'logger'
 require 'optparse'
 require 'highline/import'
 require 'csv'
 
 require_relative 'portfolio_analyzer_tools'
 require_relative 'mahara_member'
+require_relative 'portfolio_view'
 
 DEFAULT_PORTFOLIO_DOWNLOAD_DIR = "#{Dir.home}/MaharaPortfolios"
 
@@ -32,6 +34,11 @@ module PortfolioReanalyzer
     ask('> ') { |q| q.default = default_value; q.echo = echo }
   end
 
+  # set up logger
+  logger = Logger.new(STDOUT)
+  #logger.level = Logger::WARN
+  logger.level = Logger::DEBUG
+
   # parse options
   options = {}
   OptionParser.new do |opt|
@@ -39,27 +46,24 @@ module PortfolioReanalyzer
     opt.on('-a', '--download_images') { |o| options[:download_images] = "y" }
   end.parse!
 
-  say "download images: #{options[:download_images]}"
+  logger.info "download images: #{options[:download_images]}"
 
   download_images = get_parameter_from_option_or_ask(options[:download_images], "Download uploaded view images? : ", "n") == "y"
 
-  say "downloading view images ..." if download_images
+  logger.info "downloading view images ..." if download_images
 
   portfolio_download_dir = get_parameter_from_option_or_ask(options[:local_dir], "Please enter the directory for local member portfolios storage:", DEFAULT_PORTFOLIO_DOWNLOAD_DIR)
-  say "using portfolio download dir '#{portfolio_download_dir}'"
+  logger.info "using portfolio download dir '#{portfolio_download_dir}'"
 
   unless Dir.exists? portfolio_download_dir then
-    say "ERROR: local portfolio directory '#{portfolio_download_dir}' not found!"
+    logger.error "ERROR: local portfolio directory '#{portfolio_download_dir}' not found!"
     System.exit(1)
   end
 
   # select group for analysis
   group_entries = []
   Dir.entries(portfolio_download_dir).each do |entry|
-    puts entry
     absolute_path_name = File.expand_path(entry, portfolio_download_dir)
-    is_dir =  File.directory?  absolute_path_name
-    puts "is_dir: #{is_dir.to_s}"
     group_entries << entry if File.directory?(absolute_path_name) and not entry[0] == "."
   end
 
@@ -70,8 +74,23 @@ module PortfolioReanalyzer
   # select group for analysis
   member_entries = []
   Dir.entries(group_dir).each do |entry|
-    member_entries << entry if File.directory?(entry)
+    path = group_dir + "/" + entry
+    if File.directory?(path) and not entry[0] == "." then
+      logger.debug "member found: #{entry}" if File.directory?(path) and not entry[0] == "."
+
+      # recreate member
+      member = MaharaMember.load path
+      member_entries << member
+    end
   end
 
+  # for all members, reconstruct the available portfolios
+  member_entries.each do |member|
+    logger.info "member: #{member.name}  #views: #{member.views.length.to_s}"
+    member.views.each do |view|
+      logger.info "view type: #{view.class.to_s}"
+      logger.info view.to_s
+    end
+  end
 
 end
